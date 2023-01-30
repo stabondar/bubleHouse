b583f1ff981baefb2f98c5c0185eb7768613d969
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-01-30 17:23:08 +0200

    new accordion

--- src/moduls/Tabs.js
+++ src/moduls/Tabs.js
@@ -1,47 +1,70 @@
 export default class Tabs
 {
   constructor() {
-    const automatically = () => {
-        let arrowLeft = $('.slider__arrow').eq(0),
-            arrowRight = $('.slider__arrow').eq(1),
-            activeLink = $('.usecase-tabs__btn.w--current'),
-            link = $('.usecase-tabs__btn'),
-            length = link.length,
-            activeIndex = activeLink.index()
+// Accordion Settings 
+const accSettings = {
+  speed: 300, // Animation speed
+  oneOpen: true, // Close all other accordion items if true
 
-            arrowRight.on('click', () => {
-                if(activeIndex != length - 1) {
-                    activeLink.next().click()
-                    } else {
-                        link.first().click()
-                    }
+  
+  classes: {
+  	accordion: 'js-accordion',
+    header: 'js-accordion-header',
+    item: 'js-accordion-item',
+    body: 'js-accordion-body',
+    active: 'active',
+  }
+};
 
-                activeLink = $('.usecase-tabs__btn.w--current')
-                activeIndex = activeLink.index()
-            })
 
-            arrowLeft.on('click', () => {
-                if(activeIndex != 0) {
-                    activeLink.prev().click()
-                    } else {
-                        link.last().click()
-                    }
+const prefix = accSettings.classes
 
-                    activeLink = $('.usecase-tabs__btn.w--current')
-                    activeIndex = activeLink.index()
-            })
-
-            $(link).each(function(){
-                let self = $(this);
-                self.on('click', () => {
-                    let indexSelf = self.index();
-                    activeIndex = indexSelf;
+const accordion = (function(){
+  const accordionElem = $(`.${prefix.accordion}`)
+  const accordionHeader = accordionElem.find(`.${prefix.header}`)
+  const accordionItem = $(`.${prefix.item}`)
+  const accordionBody = $(`.${prefix.body}`)
+  const activeClass = prefix.active
     
-                    console.log(activeIndex)
-                })
-            })
+  return {
+    // pass configurable object literal
+    init: function(settings) {
+      accordionHeader.on('click', function() {
+        accordion.toggle($(this));
+        if(accSettings.offsetAnchor) {
+        	setTimeout(() => { 
+        		$('html').animate({scrollTop: $(this).offset().top-accSettings.offsetFromTop}, accSettings.speed);
+    			}, accSettings.scrollTopDelay);
         }
-        automatically()
+      });
+      
+      $.extend(accSettings, settings); 
+      
+      // ensure only one accordion is active if oneOpen is true
+      if(settings.oneOpen && $(`.${prefix.item}.${activeClass}`).length > 1) {
+        $(`.${prefix.item}.${activeClass}:not(:first)`).removeClass(activeClass).find(`.${prefix.header} > .${prefix.icon}`).removeClass(activeClass);
+      }
+      // reveal the active accordion bodies
+      $(`.${prefix.item}.${activeClass}`).find(`> .${prefix.body}`).show();
+      
+    },
+    
+    toggle: function($this) {
+      if(accSettings.oneOpen && $this[0] != $this.closest(accordionElem).find(`> .${prefix.item}.${activeClass} > .${prefix.header}`)[0]) {
+        $this.closest(accordionElem).find(`> .${prefix.item}`).removeClass(activeClass).find(accordionBody).slideUp(accSettings.speed);
+        $this.closest(accordionElem).find(`> .${prefix.item}`).find(`> .${prefix.header} > .${prefix.icon}`).removeClass(activeClass);
+      }
+      
+      // show/hide the clicked accordion item
+      $this.closest(accordionItem).toggleClass(`${activeClass}`).find(`> .${prefix.header} > .${prefix.icon}`).toggleClass(activeClass);
+      $this.next().stop().slideToggle(accSettings.speed);
+    }
+  }
+})();
+
+$(document).ready(function(){
+  accordion.init(accSettings);
+});
   }
 }
 
