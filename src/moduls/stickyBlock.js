35b64a1703d225cf5865ef5b6a71262c87c75262
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-04-03 18:12:55 +0300

    color

--- src/moduls/stickyBlock.js
+++ src/moduls/stickyBlock.js
@@ -36,8 +36,8 @@ export default class Sticky
 
                 const mask1 = () => {
                     const item = $('.scroll-cards__mask');
-                    const bg1 = "linear-gradient(180deg, #FFF 26.43%, rgba(243, 243, 243, 0) 34.39%, rgba(243, 243, 243, 0) 66.46%, #F3F3F3 87.03%)";
-                    const bg2 = "linear-gradient(180deg, #FFF 18.22%, rgba(243, 243, 243, 0) 21.58%, rgba(243, 243, 243, 0) 61.38%, #F3F3F3 76.86%)";
+                    const bg1 = "linear-gradient(180deg, #FFF 26.43%, rgba(243, 243, 243, 0) 34.39%, rgba(243, 243, 243, 0) 66.46%, #FFF 87.03%)";
+                    const bg2 = "linear-gradient(180deg, #FFF 18.22%, rgba(243, 243, 243, 0) 21.58%, rgba(243, 243, 243, 0) 61.38%, #FFF 76.86%)";
 
                     let tl = gsap.timeline({
                         scrollTrigger : {
